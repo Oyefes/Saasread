90c3850f310535b2f0631593706ce2815b6a57c1
oyefeso joshua <oyefesojoshua@email.com>
2024-08-31 00:29:09 +0100

    mobile phone bug fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -55,35 +55,64 @@ const Navbar = ({
               document
                 .querySelector(".form")!
                 .classList.remove("max-md:hidden");
+              setTimeout(() => {
+                document
+                  .querySelector(".form")!
+                  .classList.remove("max-md:opacity-0");
+                document
+                  .querySelector(".form")!
+                  .classList.add("max-md:opacity-100");
+              }, 100);
             }}
             className={` text-slate-950 md:hidden flex items-center justify-center bg-white  h-8 md:w-[5%] w-8 z-auto  md:min-w-8 rounded-full md:rounded-l-3xl`}
           >
             <SearchIcon className="relative w-8/12 font-bold" />
           </button>
           <form
-            className={`form max-md:before:w-screen max-md:before:h-screen max-md:before:bg-white max-md:before:absolute max-md:before:right-0 max-md:before:top-0 dark:max-md:before:bg-slate-950 max-md:before:translate-x-14 max-md:before:-translate-y-10 max-md:before:opacity-90 max-md:-translate-y-5  max-md:hidden max-md:absolute max-md:right-12 max-md:w-80 relative w-full md:flex `}
+            className={`form max-md:before:w-screen max-md:before:h-screen max-md:before:bg-white max-md:before:absolute  max-md:before:top-0 
+              max-md:w-screen
+              dark:max-md:before:bg-slate-950 max-md:before:translate-x-14
+              max-md:opacity-0
+              transition-[opacity] duration-500
+              max-md:before:-translate-y-10 max-md:before:opacity-90 max-md:-translate-y-5  max-md:hidden max-md:right-10  relative w-full md:flex max-md:absolute`}
           >
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                document.querySelector(".form")!.classList.add("max-md:hidden");
-              }}
-              className="  md:hidden flex items-center hover:border-[2px] border-slate-950 dark:border-white dark:text-white pointer justify-center  h-8 w-8 ml-auto font-extrabold rounded-md translate-x-2 text-xl my-2 "
-            >
-              <span className="relative text-slate-950 font-bold">&times;</span>
-            </button>
-            <div className="flex w-full relative border-2 rounded-3xl border-slate-950">
-              <input
-                type="text"
-                name="search"
-                placeholder="Search for a book."
-                className={`text-slate-950 search bg-white placeholder:text-sm placeholder:tracking-widest placeholder:font-serif outline-none relative w-[95%] font-semibold p-2 max-h-8 rounded-l-3xl `}
-              />
-              <button
-                className={` text-slate-950 flex items-center justify-center bg-white  h-8 md:w-[5%] w-8  md:min-w-8 rounded-full rounded-l-3xl`}
-              >
-                <SearchIcon className="relative w-8/12 font-bold" />
-              </button>
+            <div className="flex max-md:px-10 max-md:relative w-full max-md:translate-x-10 max-md:flex-col">
+              <div>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTimeout(() => {
+                      document
+                        .querySelector(".form")!
+                        .classList.add("max-md:hidden");
+                    }, 500);
+                    document
+                      .querySelector(".form")!
+                      .classList.add("max-md:opacity-0");
+                    document
+                      .querySelector(".form")!
+                      .classList.remove("max-md:opacity-100");
+                  }}
+                  className="  md:hidden flex items-center hover:border-[2px] border-slate-950 dark:border-white dark:text-white pointer justify-center  h-8 w-8 ml-auto font-extrabold rounded-md translate-x-2 text-xl my-2 "
+                >
+                  <span className="relative dark:text-white text-slate-950 font-bold">
+                    &times;
+                  </span>
+                </button>
+              </div>
+              <div className="flex w-full relative border-2 rounded-3xl border-slate-950">
+                <input
+                  type="text"
+                  name="search"
+                  placeholder="Search for a book."
+                  className={`text-slate-950 search bg-white placeholder:text-sm placeholder:tracking-widest placeholder:font-serif outline-none relative w-[95%] font-semibold p-2 max-h-8 rounded-l-3xl `}
+                />
+                <button
+                  className={` text-slate-950 flex items-center justify-center bg-white  h-8 md:w-[5%] w-8  md:min-w-8 rounded-full rounded-l-3xl`}
+                >
+                  <SearchIcon className="relative w-8/12 font-bold" />
+                </button>
+              </div>{" "}
             </div>
           </form>
         </div>
